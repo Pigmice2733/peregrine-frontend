68bd6fee9d933a66ba6797910ce0d76ad0dc31f3
Caleb Eby <caleb.eby01@gmail.com>
2018-09-30 18:57:16 -0700

    Fix rollup postcss

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,10 @@ const node = require('rollup-plugin-node-resolve')
 const { terser } = require('rollup-plugin-terser')
 const babel = require('rollup-plugin-babel')
 const postcss = require('rollup-plugin-postcss')
+const postcssConfig = require('./postcss.config')
+const cssModulesConfig = postcssConfig.plugins['postcss-modules']
+
+console.log(postcssConfig)
 
 const extensions = ['.js', '.jsx', '.es6', '.es', '.mjs', '.ts', '.tsx', '.css']
 
@@ -16,7 +20,19 @@ module.exports = {
       extensions,
       jsnext: true,
     }),
-    postcss({ extract: 'dist/style.css', minimize: true }),
+    postcss({
+      extract: 'dist/style.css',
+      modules: cssModulesConfig,
+      plugins: Object.entries(postcssConfig.plugins).reduce(
+        (plugins, [key, value]) =>
+          key === 'postcss-modules'
+            ? plugins
+            : plugins.concat(require(key)(value)),
+        [],
+      ),
+      config: false,
+      minimize: true,
+    }),
     babel({ extensions }),
     terser({
       compress: {
