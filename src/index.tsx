7f81cd15baf73c06386ba2c57ff4744f433ac407
Caleb Eby <caleb.eby01@gmail.com>
2018-10-01 13:16:39 -0700

    Set up preact devtools in development

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { h, render } from 'preact'
 import { Router, Route } from 'preact-router'
 import Home from './routes/home'
 import './style'
+import { initDevTools } from 'preact/devtools/devtools'
 
 const App = () => (
   <Router>
@@ -14,3 +15,7 @@ while (document.body.lastChild) {
 }
 
 render(<App />, document.body, document.body.lastElementChild || undefined)
+
+if (process.env.NODE_ENV !== 'production') {
+  initDevTools()
+}
