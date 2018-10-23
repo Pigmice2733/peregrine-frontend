ddfb05620f9c29fd66a14ea82ddfa3619a9ab37c
Caleb Eby <caleb.eby01@gmail.com>
2018-10-23 08:55:54 -0700

    Use icon as key in order to resolve issue with keys being VNodes which caused Preact to be buggy

--- src/components/info-group-card/index.tsx
+++ src/components/info-group-card/index.tsx
@@ -23,7 +23,7 @@ const InfoGroupCard = ({ info }: Props) => (
     {info.filter(isTruthy).map(({ icon, action, title, ...i }) => {
       const El = i.href ? 'a' : 'div'
       return (
-        <El class={style.row} key={title} {...i}>
+        <El class={style.row} key={icon} {...i}>
           <Icon icon={icon} />
           <div class={style.title}>{title}</div>
           {action && <div>{action}</div>}
