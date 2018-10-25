b22844445f665b4c872267bbec84669e8abb2c02
Caleb Eby <caleb.eby01@gmail.com>
2018-10-24 20:41:51 -0700

    Extract utils

---
+++ src/utils/format-time/index.test.ts
@@ -0,0 +1,9 @@
+import { formatTime } from '.'
+
+describe('formatTime', () => {
+  it('works with a string', () => {
+    expect(formatTime('2018-04-19T19:37:49Z', 'America/Los_Angeles')).toEqual(
+      'Thu 12:37 PM',
+    )
+  })
+})
