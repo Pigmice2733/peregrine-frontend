330b228fc5ea98c6071b719403da32ee61d07a5f
Caleb Eby <caleb.eby01@gmail.com>
2018-12-08 17:11:44 -0800

    Rename getYearSchema

--- src/api/schema/get-year-schema.ts
+++ src/api/schema/get-year-schema.ts
@@ -2,5 +2,5 @@ import { request } from '../base'
 import { Schema } from '.'
 
 // Anyone can view the standard schema for a specific year's game
-export const getYearSchemaId = (year: number) =>
+export const getYearSchema = (year: number) =>
   request<Schema>('GET', `schemas/year/${year}`)
