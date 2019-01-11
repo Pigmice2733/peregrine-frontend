ae06e709aa15dda3bd29856de45397132ef35905
Franklin Harding <franklinharding0.0@gmail.com>
2019-01-11 13:32:40 -0800

    Add no matches yet message

--- src/routes/event/index.tsx
+++ src/routes/event/index.tsx
@@ -187,17 +187,22 @@ const Event = ({ eventKey }: Props) => (
             },
             {
               name: 'Matches',
-              contents: matches ? (
-                matches.map(m => (
-                  <MatchCard
-                    key={m.key}
-                    match={m}
-                    href={`/events/${eventKey}/matches/${m.key}`}
-                  />
-                ))
-              ) : (
-                <Spinner />
-              ),
+              contents:
+                matches !== undefined ? (
+                  matches.length === 0 ? (
+                    <div class={style.noMatches}>No matches yet</div>
+                  ) : (
+                    matches.map(m => (
+                      <MatchCard
+                        key={m.key}
+                        match={m}
+                        href={`/events/${eventKey}/matches/${m.key}`}
+                      />
+                    ))
+                  )
+                ) : (
+                  <Spinner />
+                ),
             },
           ]}
         />
