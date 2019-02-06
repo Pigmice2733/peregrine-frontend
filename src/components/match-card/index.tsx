bcb77bedb6b9db93397521ef154698e0e98a0ca2
Caleb Eby <calebeby@users.noreply.github.com>
2019-02-05 16:18:37 -0800

    Reduce dom nodes (#252)

--- src/components/match-card/index.tsx
+++ src/components/match-card/index.tsx
@@ -21,7 +21,7 @@ export const MatchCard = ({ match, ...rest }: MatchCardProps) => {
   return (
     <Card class={style.matchCard} {...rest}>
       <div class={style.matchTitle}>
-        <div>{matchName.group}</div>
+        {matchName.num ? <div>{matchName.group}</div> : matchName.group}
         {matchName.num && (
           <div class={style.matchNum}>{`Match ${matchName.num}`}</div>
         )}
@@ -32,14 +32,10 @@ export const MatchCard = ({ match, ...rest }: MatchCardProps) => {
         </time>
       )}
       <div class={`${style.red} ${style.alliance}`}>
-        {match.redAlliance.map(t => (
-          <div key={t}>{formatTeamNumber(t)}</div>
-        ))}
+        {match.redAlliance.map(t => formatTeamNumber(t)).join(' ')}
       </div>
       <div class={`${style.blue} ${style.alliance}`}>
-        {match.blueAlliance.map(t => (
-          <div key={t}>{formatTeamNumber(t)}</div>
-        ))}
+        {match.blueAlliance.map(t => formatTeamNumber(t)).join(' ')}
       </div>
     </Card>
   )
