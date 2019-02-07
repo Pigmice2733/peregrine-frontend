bf5b5d0487db9c3d53a0653faf65914432ac1e55
Caleb Eby <caleb.eby01@gmail.com>
2019-02-06 20:01:00 -0800

    REGEX!

--- src/utils/format-time/index.test.ts
+++ src/utils/format-time/index.test.ts
@@ -2,6 +2,8 @@ import { formatTime } from '.'
 
 describe('formatTime', () => {
   it('works with a string', () => {
-    expect(formatTime(new Date('2018-04-19T19:37:49Z'))).toEqual('Thu 12:37 PM')
+    expect(formatTime(new Date('2018-04-19T19:37:49Z'))).toMatch(
+      /\w\w\w \d\d?:\d\d [PA]M/,
+    )
   })
 })
