bb2b6a5e0f88dffc68cca762cc0e15d824bcdf74
Caleb Eby <calebeby@users.noreply.github.com>
2019-05-17 22:29:37 -0700

    Update card styles to more closely match MD spec (#361)

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,13 +1,35 @@
 import { h, JSX } from 'preact'
-import style from './style.css'
 import { Merge } from '@/type-utils'
 import clsx from 'clsx'
+import { css } from 'linaria'
+import { createShadow } from '@/utils/create-shadow'
+import { pxToRem } from '@/utils/px-to-rem'
+
+const cardStyle = css`
+  border-radius: ${pxToRem(4)};
+  box-shadow: ${createShadow(1)};
+  background: white;
+  text-decoration: none;
+  color: inherit;
+  transition: all 0.3s ease;
+
+  &[href]:hover,
+  &:focus:not(:active),
+  &:focus-within:not(:active) {
+    box-shadow: ${createShadow(8)};
+  }
+
+  &:focus {
+    outline: none;
+    background: #dfdfdf;
+  }
+`
 
 export type CardProps<T = {}> = Merge<JSX.HTMLAttributes, T>
 
 const Card = (props: JSX.HTMLAttributes) => {
   const El = props.href ? 'a' : 'div'
-  return <El {...props} class={clsx(style.card, props.class)} />
+  return <El {...props} class={clsx(cardStyle, props.class)} />
 }
 
 export default Card
