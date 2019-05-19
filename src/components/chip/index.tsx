877452e6088e4e1b11175fd1d4f57222695745e5
Caleb Eby <caleb.eby01@gmail.com>
2019-05-18 18:15:27 -0700

    Use faster date formatter cuz fast

--- src/components/chip/index.tsx
+++ src/components/chip/index.tsx
@@ -1,15 +1,19 @@
 import { h } from 'preact'
 import style from './style.css'
 
-const formatDate = (d: Date) =>
-  d.toLocaleDateString('en-US', { month: 'short', day: 'numeric' })
+const dateFormatter = new Intl.DateTimeFormat('en-US', {
+  month: 'short',
+  day: 'numeric',
+})
 
 const Chip = ({ children }: { children: string }) => (
   <span class={style.chip}>{children}</span>
 )
 
 export const DateChip = ({ date }: { date: Date }) => (
-  <span class={`${style.dateChip} ${style.chip}`}>{formatDate(date)}</span>
+  <span class={`${style.dateChip} ${style.chip}`}>
+    {dateFormatter.format(date)}
+  </span>
 )
 
 export default Chip
