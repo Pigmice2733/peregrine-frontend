11b2bff698195df6d8e88855ab5a4fb46d0a6796
Caleb Eby <caleb.eby01@gmail.com>
2019-05-18 17:24:44 -0700

    Add fake indexeddb for testing

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,15 +1,16 @@
 import { cleanup } from 'preact-testing-library'
 import fetch, { Response, Headers } from 'node-fetch'
 import 'jest-dom/extend-expect'
+import 'fake-indexeddb/auto'
 import { removeAccessToken, removeRefreshToken } from '@/jwt'
 
 window.fetch = (fetch as unknown) as GlobalFetch['fetch']
 ;(window as any).Response = Response
 ;(window as any).Headers = Headers
 
-afterEach(cleanup)
-
 afterEach(() => {
+  cleanup()
+  indexedDB.deleteDatabase('CACHE')
   removeAccessToken()
   removeRefreshToken()
   localStorage.clear()
