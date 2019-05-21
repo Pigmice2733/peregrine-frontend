30dec29bb361aaf7f42c8f5f07b33d721542c87b
Caleb Eby <caleb.eby01@gmail.com>
2019-05-21 14:26:42 -0700

    Add more specific types

--- src/utils/compare-matches/index.test.ts
+++ src/utils/compare-matches/index.test.ts
@@ -5,6 +5,6 @@ test.each([
   ['qf1m1', 'qf3m2', -1],
   ['qf3m1', 'f3m2', -1],
   ['sf3m1', 'sf4m1', -1],
-])('compare %s and %s', (a, b, expected) => {
+] as [string, string, -1 | 1][])('compare %s and %s', (a, b, expected) => {
   expect(Math.sign(compareMatches({ key: a }, { key: b }))).toEqual(expected)
 })
