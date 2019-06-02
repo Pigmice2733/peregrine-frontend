69c9757648c9161a9edcfdf3e55765d61dff34b6
Caleb Eby <caleb.eby01@gmail.com>
2019-06-01 19:33:08 -0700

    Switch API URL back to frank's

--- src/api/api-url.ts
+++ src/api/api-url.ts
@@ -1,5 +1,5 @@
 export const apiUrl =
   (process.env.PEREGRINE_API_URL ||
     (process.env.NODE_ENV === 'production' && process.env.BRANCH === 'master'
-      ? 'https://peregrinek8s.xyz'
-      : 'https://peregrinek8s.xyz')) + '/'
+      ? 'https://api.peregrine.ga'
+      : 'https://api.peregrine.ga')) + '/'
