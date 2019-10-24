fddd089fc91556178ee4553b2afa72d0bbf73a83
Marek <marek.jarab@student.tuke.sk>
2019-10-24 18:39:31 +0200

    application of suggestions

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -6,39 +6,31 @@ import { compareEvents } from '@/utils/compare-events'
 import { useGeoLocation } from '@/utils/use-geo-location'
 import { useEvents } from '@/cache/events/use'
 import { useState } from 'preact/hooks'
+import { ProcessedEventInfo } from '@/api/event-info'
 
 const now = new Date()
 const Home = () => {
   const events = useEvents()
   const location = useGeoLocation()
-  const [term = '', setTerm] = useState(0);
+  const [term = '', setTerm] = useState('');
+  const lowerCaseQuery = term.toString().toLowerCase();
 
-  function searchingFor(term: string | number) {
-    return function (x: any) {
-      // because not all objects contain properties 'district' and 'fullDistrict' you have to check if particular object contains property 'distict' and if not tell method to not use it. Otherwise this error will occur (Uncaught (in promise) TypeError: Cannot read property 'toString' of undefined)
-      if (x.hasOwnProperty('district')) {
-        return (x.name.toString().toLowerCase().includes(term.toString().toLowerCase()) || !term) ||
-          (x.key.toString().toLowerCase().includes(term.toString().toLowerCase()) || !term) ||
-          (x.locationName.toString().toLowerCase().includes(term.toString().toLowerCase()) || !term) ||
-          (x.district.toString().toLowerCase().includes(term.toString().toLowerCase()) || !term) ||
-          (x.fullDistrict.toString().toLowerCase().includes(term.toString().toLowerCase()) || !term);
-      }
-
-      return (x.name.toString().toLowerCase().includes(term.toString().toLowerCase()) || !term) ||
-        (x.key.toString().toLowerCase().includes(term.toString().toLowerCase()) || !term) ||
-        (x.locationName.toString().toLowerCase().includes(term.toString().toLowerCase()) || !term);
-
+  function searchingFor(term: string) {
+    return function (event: ProcessedEventInfo) {
+      if (!term) return true;
+      return (event.name.toLowerCase().includes(lowerCaseQuery)) ||
+        (event.key.toLowerCase().includes(lowerCaseQuery)) ||
+        (event.locationName.toLowerCase().includes(lowerCaseQuery)) ||
+        (event.district !== undefined && event.district.toLowerCase().includes(lowerCaseQuery)) ||
+        (event.fullDistrict !== undefined && event.fullDistrict.toLowerCase().includes(lowerCaseQuery));
     }
   }
-  function searchHandler(event: any) {
-    setTerm(event.target.value);
-  }
+
   return (
     <Page name="Home" back={false}>
       <div>Search bar</div>
       <div class="searchBar">
-        <input type="text" onInput={(e) => searchHandler(e)}
-        />
+        <input type="text" onInput={(e: any) => setTerm(e.target.value)} />
       </div>
       <div>
         {events ? (
