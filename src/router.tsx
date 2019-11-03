cfeb977a42a0dc447126461a162e7a09a556bb8f
Caleb Eby <calebeby@users.noreply.github.com>
2019-11-02 22:05:04 -0700

    Simplify router and fix unmount bug (#621)

--- src/router.tsx
+++ src/router.tsx
@@ -14,20 +14,13 @@ interface Route {
   component: () => Promise<ComponentModule>
 }
 
-interface URLComponentPromiseMap {
-  [url: string]: () => Promise<ComponentModule>
-}
-
-interface URLComponentMap {
-  [url: string]: AnyComponent
-}
-
 const routers: ((url: string) => void)[] = []
 
 export const route = (url: string) => {
   routers.forEach(router => {
     router(url)
   })
+  history.pushState(null, '', url)
 }
 
 export const Router = ({
@@ -38,30 +31,13 @@ export const Router = ({
   onChange: () => void
 }) => {
   const [url, setUrl] = useState(window.location.pathname)
-  const [resolvedComponentMap, setResolvedComponentMap] = useState<
-    URLComponentMap
-  >({})
-
-  const updateUrl = (url: string) => {
-    setUrl(url)
-    history.pushState(null, '', url)
-  }
-
-  const components = useMemo(
-    () =>
-      routes.reduce<URLComponentPromiseMap>((acc, r) => {
-        acc[r.path] = r.component
-        return acc
-      }, {}),
-    [routes],
-  )
 
   const parsedRoutes = useMemo(() => routes.map(route => parse(route.path)), [
     routes,
   ])
 
   useEffect(() => {
-    routers.push(updateUrl)
+    routers.push(setUrl)
   }, [])
 
   useEffect(() => {
@@ -97,7 +73,7 @@ export const Router = ({
 
       // if link is handled by the router, prevent browser defaults
       if (match(href, parsedRoutes).length !== 0) {
-        updateUrl(href)
+        route(href)
         e.preventDefault()
         e.stopImmediatePropagation()
         e.stopPropagation()
@@ -110,26 +86,30 @@ export const Router = ({
     return () => window.removeEventListener('click', clickListener)
   }, [parsedRoutes])
 
-  const matchingRoute = match(url, parsedRoutes)
+  const matchingRoutes = match(url, parsedRoutes)
+  const matchingFullRoute =
+    matchingRoutes.length > 0 ? matchingRoutes[0].old : null
 
-  if (matchingRoute.length === 0) {
+  const [
+    ResolvedComponent,
+    setResolvedComponent,
+  ] = useState<AnyComponent | null>(null)
+
+  useEffect(() => {
+    const matchingRouteObj = routes.find(r => r.path === matchingFullRoute)
+    if (matchingRouteObj)
+      matchingRouteObj.component().then(comp => {
+        setResolvedComponent(() => comp.default)
+      })
+  }, [routes, matchingFullRoute])
+
+  if (matchingFullRoute === null) {
     return <h1>404</h1>
   }
 
-  const matchingFullRoute = matchingRoute[0].old
-
-  const routeProps = exec(url, matchingRoute)
-
-  const ResolvedComponent = resolvedComponentMap[matchingFullRoute]
+  const routeProps = exec(url, matchingRoutes)
 
   if (ResolvedComponent) return <ResolvedComponent {...routeProps} />
-
-  components[matchingFullRoute]().then(c => {
-    setResolvedComponentMap(prevMap => ({
-      ...prevMap,
-      [matchingFullRoute]: c.default,
-    }))
-  })
 
   return <Spinner />
 }
