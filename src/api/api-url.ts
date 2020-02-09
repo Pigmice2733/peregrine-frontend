66c941a07d242591c99245f3fcbcb54c28cb5feb
Caleb Eby <caleb.eby01@gmail.com>
2020-02-08 16:40:29 -0800

    Use API proxy

--- src/api/api-url.ts
+++ src/api/api-url.ts
@@ -1,5 +1,8 @@
+const PROD_URL = '/api'
+const DEV_URL = PROD_URL
+
+const isProd =
+  process.env.NODE_ENV === 'production' && process.env.BRANCH === 'master'
+
 export const apiUrl =
-  (process.env.PEREGRINE_API_URL ||
-    (process.env.NODE_ENV === 'production' && process.env.BRANCH === 'master'
-      ? 'https://api.peregrine.ga'
-      : 'https://api.peregrine.ga')) + '/'
+  (process.env.PEREGRINE_API_URL || (isProd ? PROD_URL : DEV_URL)) + '/'
