226a51801794664915ef40554c1380c7bca945f3
Caleb Eby <caleb.eby01@gmail.com>
2020-02-18 11:16:57 -0800

    Create special icon for apple with padding and background

--- rollup.config.js
+++ rollup.config.js
@@ -52,66 +52,66 @@ const babelOptions = { extensions, babelrc: false, ...babelConfig }
 mkdirplz(outDir)
 
 export default [
-  {
-    input: './src/index.tsx',
-    output: {
-      dir: 'dist',
-      format: 'esm',
-      preferConst: true,
-      sourcemap: false,
-      chunkFileNames: '[hash].js',
-    },
-    experimentalOptimizeChunks: true,
-    chunkGroupingSize: 50000,
-    plugins: [
-      node(rollupNodeOptions),
-      linaria({ sourceMap: false }),
-      postcss({
-        extract: 'dist/style.css',
-        modules: cssModulesConfig,
-        plugins: Object.entries(postcssPlugins).reduce(
-          (plugins, [key, value]) =>
-            key === 'postcss-modules'
-              ? plugins
-              : plugins.concat(require(key)(value)),
-          [],
-        ),
-        config: false,
-        minimize: { zindex: false },
-      }),
-      babel(babelOptions),
-      terser(terserOptions(prod)),
-      netlifyPush({
-        getRoutes: () => parseRoutes('./src/routes.ts'),
-        resolveFrom: './src/routes.ts',
-        everyRouteHeaders: [printPush({ path: '/style.css', as: 'style' })],
-        everyRouteModules: ['./index.tsx'],
-      }),
-      {
-        name: 'rollup-write-html',
-        async writeBundle() {
-          const htmlSrc = await readFileAsync('rollup-index.html', 'utf8')
-          const htmlOut = templite(htmlSrc, { apiUrl })
-          writeFileAsync(join(outDir, 'index.html'), htmlOut)
-        },
-      },
-      {
-        name: 'rollup-plugin-chunks-json',
-        async writeBundle(bundle) {
-          const chunksJSON = Object.values(bundle)
-            .filter(chunk => !chunk.isAsset)
-            .map(chunk => `/${chunk.fileName}`)
-          await writeFileAsync(chunksFile, JSON.stringify(chunksJSON))
-        },
-      },
-      {
-        name: 'rollup-plugin-copy',
-        async writeBundle() {
-          await cpy('_redirects', outDir)
-        },
-      },
-    ],
-  },
+  // {
+  //   input: './src/index.tsx',
+  //   output: {
+  //     dir: 'dist',
+  //     format: 'esm',
+  //     preferConst: true,
+  //     sourcemap: false,
+  //     chunkFileNames: '[hash].js',
+  //   },
+  //   experimentalOptimizeChunks: true,
+  //   chunkGroupingSize: 50000,
+  //   plugins: [
+  //     node(rollupNodeOptions),
+  //     linaria({ sourceMap: false }),
+  //     postcss({
+  //       extract: 'dist/style.css',
+  //       modules: cssModulesConfig,
+  //       plugins: Object.entries(postcssPlugins).reduce(
+  //         (plugins, [key, value]) =>
+  //           key === 'postcss-modules'
+  //             ? plugins
+  //             : plugins.concat(require(key)(value)),
+  //         [],
+  //       ),
+  //       config: false,
+  //       minimize: { zindex: false },
+  //     }),
+  //     babel(babelOptions),
+  //     terser(terserOptions(prod)),
+  //     netlifyPush({
+  //       getRoutes: () => parseRoutes('./src/routes.ts'),
+  //       resolveFrom: './src/routes.ts',
+  //       everyRouteHeaders: [printPush({ path: '/style.css', as: 'style' })],
+  //       everyRouteModules: ['./index.tsx'],
+  //     }),
+  //     {
+  //       name: 'rollup-write-html',
+  //       async writeBundle() {
+  //         const htmlSrc = await readFileAsync('rollup-index.html', 'utf8')
+  //         const htmlOut = templite(htmlSrc, { apiUrl })
+  //         writeFileAsync(join(outDir, 'index.html'), htmlOut)
+  //       },
+  //     },
+  //     {
+  //       name: 'rollup-plugin-chunks-json',
+  //       async writeBundle(bundle) {
+  //         const chunksJSON = Object.values(bundle)
+  //           .filter(chunk => !chunk.isAsset)
+  //           .map(chunk => `/${chunk.fileName}`)
+  //         await writeFileAsync(chunksFile, JSON.stringify(chunksJSON))
+  //       },
+  //     },
+  //     {
+  //       name: 'rollup-plugin-copy',
+  //       async writeBundle() {
+  //         await cpy('_redirects', outDir)
+  //       },
+  //     },
+  //   ],
+  // },
   {
     input: './src/sw.ts',
     output: {
@@ -135,16 +135,16 @@ export default [
       node(rollupNodeOptions),
       babel(babelOptions),
       terser(terserOptions(prod)),
-      {
-        name: 'write-manifest',
-        async writeBundle() {
-          const manifestSrc = await readFileAsync('./src/manifest.json', 'utf8')
-          await writeFileAsync(
-            join(outDir, 'manifest.json'),
-            JSON.stringify(JSON.parse(manifestSrc)),
-          )
-        },
-      },
+      // {
+      //   name: 'write-manifest',
+      //   async writeBundle() {
+      //     const manifestSrc = await readFileAsync('./src/manifest.json', 'utf8')
+      //     await writeFileAsync(
+      //       join(outDir, 'manifest.json'),
+      //       JSON.stringify(JSON.parse(manifestSrc)),
+      //     )
+      //   },
+      // },
       {
         name: 'write-icons',
         async writeBundle() {
@@ -152,17 +152,39 @@ export default [
           const iconDir = join(outDir, 'icons')
           await mkdirplz(iconDir)
           const background = 'transparent'
-          await Promise.all(
-            [512, 192, 180, 32, 16].map(async width =>
+          const appleBg = '#800080'
+          const appleWidth = 180
+          const applePad = Math.round(0.07 * appleWidth)
+          await Promise.all([
+            ...[512, 192, 180, 32, 16].map(
+              async width =>
+                writeFileAsync(
+                  join(iconDir, `${width}.png`),
+                  await sharp(iconSrc)
+                    .resize(width, width, { fit: 'contain', background })
+                    .png()
+                    .toBuffer(),
+                ),
               writeFileAsync(
-                join(iconDir, `${width}.png`),
+                join(iconDir, `apple.png`),
                 await sharp(iconSrc)
-                  .resize(width, width, { fit: 'contain', background })
+                  .resize(appleWidth - applePad * 2, 180 - applePad * 2, {
+                    fit: 'contain',
+                    background: appleBg,
+                  })
+                  .extend({
+                    top: applePad,
+                    bottom: applePad,
+                    right: applePad,
+                    left: applePad,
+                    background: appleBg,
+                  })
+                  .flatten({ background: appleBg })
                   .png()
                   .toBuffer(),
               ),
             ),
-          )
+          ])
         },
       },
     ],
