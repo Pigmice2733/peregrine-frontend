2c12cc5167b548fc98bf4937d0625a4648d2707c
Caleb Eby <caleb.eby01@gmail.com>
2020-02-18 11:19:01 -0800

    Uncomment

--- rollup.config.js
+++ rollup.config.js
@@ -52,66 +52,66 @@ const babelOptions = { extensions, babelrc: false, ...babelConfig }
 mkdirplz(outDir)
 
 export default [
-  // {
-  //   input: './src/index.tsx',
-  //   output: {
-  //     dir: 'dist',
-  //     format: 'esm',
-  //     preferConst: true,
-  //     sourcemap: false,
-  //     chunkFileNames: '[hash].js',
-  //   },
-  //   experimentalOptimizeChunks: true,
-  //   chunkGroupingSize: 50000,
-  //   plugins: [
-  //     node(rollupNodeOptions),
-  //     linaria({ sourceMap: false }),
-  //     postcss({
-  //       extract: 'dist/style.css',
-  //       modules: cssModulesConfig,
-  //       plugins: Object.entries(postcssPlugins).reduce(
-  //         (plugins, [key, value]) =>
-  //           key === 'postcss-modules'
-  //             ? plugins
-  //             : plugins.concat(require(key)(value)),
-  //         [],
-  //       ),
-  //       config: false,
-  //       minimize: { zindex: false },
-  //     }),
-  //     babel(babelOptions),
-  //     terser(terserOptions(prod)),
-  //     netlifyPush({
-  //       getRoutes: () => parseRoutes('./src/routes.ts'),
-  //       resolveFrom: './src/routes.ts',
-  //       everyRouteHeaders: [printPush({ path: '/style.css', as: 'style' })],
-  //       everyRouteModules: ['./index.tsx'],
-  //     }),
-  //     {
-  //       name: 'rollup-write-html',
-  //       async writeBundle() {
-  //         const htmlSrc = await readFileAsync('rollup-index.html', 'utf8')
-  //         const htmlOut = templite(htmlSrc, { apiUrl })
-  //         writeFileAsync(join(outDir, 'index.html'), htmlOut)
-  //       },
-  //     },
-  //     {
-  //       name: 'rollup-plugin-chunks-json',
-  //       async writeBundle(bundle) {
-  //         const chunksJSON = Object.values(bundle)
-  //           .filter(chunk => !chunk.isAsset)
-  //           .map(chunk => `/${chunk.fileName}`)
-  //         await writeFileAsync(chunksFile, JSON.stringify(chunksJSON))
-  //       },
-  //     },
-  //     {
-  //       name: 'rollup-plugin-copy',
-  //       async writeBundle() {
-  //         await cpy('_redirects', outDir)
-  //       },
-  //     },
-  //   ],
-  // },
+  {
+    input: './src/index.tsx',
+    output: {
+      dir: 'dist',
+      format: 'esm',
+      preferConst: true,
+      sourcemap: false,
+      chunkFileNames: '[hash].js',
+    },
+    experimentalOptimizeChunks: true,
+    chunkGroupingSize: 50000,
+    plugins: [
+      node(rollupNodeOptions),
+      linaria({ sourceMap: false }),
+      postcss({
+        extract: 'dist/style.css',
+        modules: cssModulesConfig,
+        plugins: Object.entries(postcssPlugins).reduce(
+          (plugins, [key, value]) =>
+            key === 'postcss-modules'
+              ? plugins
+              : plugins.concat(require(key)(value)),
+          [],
+        ),
+        config: false,
+        minimize: { zindex: false },
+      }),
+      babel(babelOptions),
+      terser(terserOptions(prod)),
+      netlifyPush({
+        getRoutes: () => parseRoutes('./src/routes.ts'),
+        resolveFrom: './src/routes.ts',
+        everyRouteHeaders: [printPush({ path: '/style.css', as: 'style' })],
+        everyRouteModules: ['./index.tsx'],
+      }),
+      {
+        name: 'rollup-write-html',
+        async writeBundle() {
+          const htmlSrc = await readFileAsync('rollup-index.html', 'utf8')
+          const htmlOut = templite(htmlSrc, { apiUrl })
+          writeFileAsync(join(outDir, 'index.html'), htmlOut)
+        },
+      },
+      {
+        name: 'rollup-plugin-chunks-json',
+        async writeBundle(bundle) {
+          const chunksJSON = Object.values(bundle)
+            .filter(chunk => !chunk.isAsset)
+            .map(chunk => `/${chunk.fileName}`)
+          await writeFileAsync(chunksFile, JSON.stringify(chunksJSON))
+        },
+      },
+      {
+        name: 'rollup-plugin-copy',
+        async writeBundle() {
+          await cpy('_redirects', outDir)
+        },
+      },
+    ],
+  },
   {
     input: './src/sw.ts',
     output: {
@@ -135,16 +135,16 @@ export default [
       node(rollupNodeOptions),
       babel(babelOptions),
       terser(terserOptions(prod)),
-      // {
-      //   name: 'write-manifest',
-      //   async writeBundle() {
-      //     const manifestSrc = await readFileAsync('./src/manifest.json', 'utf8')
-      //     await writeFileAsync(
-      //       join(outDir, 'manifest.json'),
-      //       JSON.stringify(JSON.parse(manifestSrc)),
-      //     )
-      //   },
-      // },
+      {
+        name: 'write-manifest',
+        async writeBundle() {
+          const manifestSrc = await readFileAsync('./src/manifest.json', 'utf8')
+          await writeFileAsync(
+            join(outDir, 'manifest.json'),
+            JSON.stringify(JSON.parse(manifestSrc)),
+          )
+        },
+      },
       {
         name: 'write-icons',
         async writeBundle() {
