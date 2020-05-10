ba4218d48f87111af4add13afe4bbfe2e231d67d
Caleb Eby <caleb.eby01@gmail.com>
2020-05-09 17:39:05 -0700

    Switch to optional chaining (automated)

--- src/routes/event-match.tsx
+++ src/routes/event-match.tsx
@@ -72,7 +72,7 @@ const EventMatch = ({ eventKey, matchKey }: Props) => {
   )
 
   const matchHasBeenPlayed =
-    match && match.blueScore !== undefined && match.redScore !== undefined
+    match?.blueScore !== undefined && match.redScore !== undefined
 
   // When the match loads (or changes),
   useEffect(() => {
