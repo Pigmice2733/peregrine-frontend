685b136b82e4290736a45f759fde9060ca6f074d
Caleb Eby <caleb.eby01@gmail.com>
2020-05-26 00:00:15 -0700

    Undo

--- .babelrc.output.js
+++ .babelrc.output.js
@@ -1,7 +1,5 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env', { loose: true, bugfixes: true, modules: false }],
-  ],
+  presets: [['@babel/preset-env', { loose: true, bugfixes: true }]],
   plugins: [
     // Buggy right now so not being used
     //   process.env.NODE_ENV === 'production' &&
