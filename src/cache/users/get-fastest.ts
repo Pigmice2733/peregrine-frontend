e5b071a3a89e6a239c56c2e5647ff4e908671483
Caleb Eby <caleb.eby01@gmail.com>
2020-10-31 13:27:19 -0700

    Remove TODO comment

--- src/cache/users/get-fastest.ts
+++ src/cache/users/get-fastest.ts
@@ -3,6 +3,3 @@ import { createPromiseRace } from '@/utils/fastest-promise'
 import { getCachedUser } from './get-cached'
 
 export const getFastestUser = createPromiseRace(getUser, getCachedUser)
-
-// TODO: Next time work on clearing IDB when logged out
-// https://github.com/Pigmice2733/peregrine-frontend/issues/1097
