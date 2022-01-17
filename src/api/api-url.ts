4d0be37c982a5266f993530303a8e3a72f223136
Caleb Eby <caleb.eby01@gmail.com>
2022-01-16 17:57:52 -0800

    Fix API url for new domain

--- src/api/api-url.ts
+++ src/api/api-url.ts
@@ -2,4 +2,4 @@ const isNetlify = process.env.NODE_ENV === 'production' && process.env.BRANCH
 
 export const apiUrl =
   (process.env.PEREGRINE_API_URL ||
-    (isNetlify ? '/api' : 'https://peregrine.ga/api')) + '/'
+    (isNetlify ? '/api' : 'https://peregrinefrc.com/api')) + '/'
