c976016883bc0861b49a6833f672ca52d9f61cc8
var Cepheid <avhoi83@gmail.com>
2022-02-17 17:22:32 -0800

    lint

--- src/api/stats/get-event-stats.ts
+++ src/api/stats/get-event-stats.ts
@@ -1,16 +1,18 @@
 import { request } from '../base'
 import { TeamStats, processTeamStats } from '.'
-import { getYears } from '../get-years';
+import { getYears } from '../get-years'
 
 // These are the stats for every team at an event, describing their performance
 // only at that event
 export const getEventStats = async (eventKey: string, curYear: number) => {
-  const schemaYears = await getYears();
-  const definedSchema = schemaYears.includes(curYear);
+  const schemaYears = await getYears()
+  const definedSchema = schemaYears.includes(curYear)
 
-  if(definedSchema){ 
-    return request<TeamStats[]>('GET', `events/${eventKey}/stats`).then((teamStats) => {
-      return teamStats.map(processTeamStats)
-    })
+  if (definedSchema) {
+    return request<TeamStats[]>('GET', `events/${eventKey}/stats`).then(
+      (teamStats) => {
+        return teamStats.map(processTeamStats)
+      },
+    )
   }
 }
