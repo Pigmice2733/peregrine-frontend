dbc33852ec81a3d210856d7bebc6e68d8fc8e0f7
var Cepheid <avhoi83@gmail.com>
2022-04-07 17:32:43 -0700

    changed "next match" to be based on time
    Scores don't upload quickly, so this appears to fix the issue. Also ran lint.

--- src/utils/clean-field-name.ts
+++ src/utils/clean-field-name.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-warning-comments */
 // TODO: Remove once we can handle multiple fields of the same name
 export const cleanFieldName = (fieldName: string) =>
   fieldName.replace(/\((?:auto|teleop)\)/, '').trim()
--- src/utils/next-incomplete-match.ts
+++ src/utils/next-incomplete-match.ts
@@ -1,11 +1,13 @@
 import { ProcessedMatchInfo } from '@/api/match-info'
 import { compareMatches } from './compare-matches'
+import { useCurrentTime } from './use-current-time'
 
 export const nextIncompleteMatch = (matches: ProcessedMatchInfo[]) =>
   matches.reduce<ProcessedMatchInfo | null>((prev, match) => {
-    // if match is complete, it is not a candidate
-    if (match.redScore !== undefined) return prev
+    // if match has started, it is not a candidate
+    if (!match.time || match.time.getTime() < useCurrentTime().getTime())
+      return prev
     // nothing to compare against so this one must be the best so far
     if (!prev) return match
-    return compareMatches(prev, match) > 1 ? match : prev
+    return compareMatches(prev, match) > 0 ? match : prev
   }, null)
