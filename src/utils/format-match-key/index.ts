2ae5d05d74f9a1d77f4f2d4f6aca3c7e6f788038
Victor Huynh <43865837+VictorHuynh@users.noreply.github.com>
2022-06-09 15:00:34 -0700

    Added a message for non-existent matches
    from pull request #1057 
    
    * Added a message for non-existent matches
    
    * Tweaked how invalid match keys are identified
    
    * Tidied up code and undid some changes
    
    * addressed the review components
    
    The lint and type checks are finding lots of errors I didn't put there, and they won't let the webpack run, so I haven't tested any of this. I'm not sure exactly how to fix all of those errors.
    
    * error fixes
    
    * fixed lint errors
    
    * test fix?
    
    * more test fixes
    
    * fixed errors
    
    * ran type and lint
    
    * lint
    
    * fixed Fragment error
    
    * rebuilt the card that appears on non-existent match
    
    * fixed normal match-page issues
    
    * aesthetic, code-shortening, lint
    
    * undo unneeded change
    
    * undid bug-fix changes
    
    * more reverting bug fixes
    
    * attempted to differentiate between different kinds of errors NEEDS TO BE TESTED
    
    * aesthetic changes
    
    Co-authored-by: Alex <avhoi83@gmail.com>
    Co-authored-by: Caleb Eby <caleb.eby01@gmail.com>

--- src/utils/format-match-key/index.ts
+++ src/utils/format-match-key/index.ts
@@ -1,10 +1,14 @@
 import { getMatchType, matchNames } from '../match-type'
 
 export const formatMatchKey = (key: string) => {
+  // determines the type of match (qualifiers or level of playoffs)
   const matchType = matchNames[getMatchType(key)]
+  // determines the number of the match (at the end of the key)
   const matchNum = /(\d+)$/.exec(key)
-  if (!matchNum || matchType === '') return { group: key.toUpperCase() }
+  if (!matchNum || matchType === '') return { group: key.toUpperCase() } // return null if can't identify match type or number
+  // group of playoff matches, if applicable
   const groupNum = /(\d+)m\d+$/.exec(key)
+  // determine whether there is a group and return the object
   if (groupNum) {
     return { group: `${matchType} ${groupNum[1]}`, num: matchNum[1] }
   }
