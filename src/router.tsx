d666723fa190c6b790bcfaaa1bc8c8626bbe0346
varCepheid <avhoi83@gmail.com>
2022-06-09 16:12:30 -0700

    back-merged from handle-network-errors and configured the ErrorCard, still needs work that can be done on that branch --Elijah

--- src/router.tsx
+++ src/router.tsx
@@ -8,7 +8,7 @@ import { css } from 'linaria'
 import { mdiClose } from '@mdi/js'
 import Icon from './components/icon'
 import { createShadow } from './utils/create-shadow'
-import errorPage from './routes/404-error'
+import ErrorCard from './components/error-card'
 
 type AnyComponent = ComponentType<any> | ((props: any) => VNode<any> | null)
 
@@ -119,7 +119,17 @@ export const Router = ({ routes }: { routes: Route[] }) => {
         .catch(() => location.reload())
   }, [matchingRoute, matchingRouteObj, path])
 
-  if (matchingFullRoute === null) return errorPage
+  if (matchingFullRoute === null)
+    return (
+      <ErrorCard
+        errorText={'The requested URL was not found.'}
+        errorText2={
+          'Use the back button to return to the previous page, or the button below to go to the home page.'
+        }
+        buttonText={'return to home page'}
+        buttonReference={'/'}
+      />
+    )
 
   if (ResolvedComponent) {
     return (
