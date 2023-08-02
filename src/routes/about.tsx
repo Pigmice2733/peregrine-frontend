a2faebb8a73a926eba3341babcea79748c824851
Alex <avhoi83@gmail.com>
2023-08-02 10:27:53 -0700

    added more substance to about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -5,10 +5,20 @@ const pageStyle = css`
   align-content: center;
 `
 
+const textStyle = css`
+  font-size: 16pt;
+  color: black;
+  font-family: Roboto sans-serif serif;
+`
+
 const AboutPage = () => {
   return (
-    <Page name="About" back={() => window.history.back()} class={pageStyle}>
+    <Page name="About" back="/" class={pageStyle}>
       <h1>Welcome to Peregrine!</h1>
+      <div class={textStyle}>
+        Peregrine is a scouting app for FRC competitions. The frontend is
+        written in TypeScript and the backend is written in Go.
+      </div>
     </Page>
   )
 }
