80f7f1dc020a117d626d0d99c4136fc8e82645da
Alex <avhoi83@gmail.com>
2023-08-02 10:13:29 -0700

    created about page and added link from sidebar menu

---
+++ src/routes/about.tsx
@@ -0,0 +1,14 @@
+import Page from '@/components/page'
+import { css } from 'linaria'
+
+const pageStyle = css`
+  align-content: center;
+`
+
+export const AboutPage = () => {
+  return (
+    <Page name="About" back={() => window.history.back()} class={pageStyle}>
+      <h1>Welcome to Peregrine!</h1>
+    </Page>
+  )
+}
