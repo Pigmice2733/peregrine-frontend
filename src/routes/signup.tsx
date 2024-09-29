7ec96087dae575343d0770a7d63b8b6cd9f1a6f0
Alex <avhoi83@gmail.com>
2024-09-28 20:46:21 -0700

    create alphabetical sorting function for realms dropdown

--- src/routes/signup.tsx
+++ src/routes/signup.tsx
@@ -49,6 +49,8 @@ const SignUpForm = () => {
   const realms = usePromise(getRealms) || []
   const emitError = useErrorEmitter()
 
+  realms.sort((a, b) => a.name.localeCompare(b.name))
+
   const onSubmit = (e: Event) => {
     e.preventDefault()
     if (realmId === undefined) return
