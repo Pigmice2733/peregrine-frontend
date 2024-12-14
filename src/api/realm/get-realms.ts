f996552b676f38365b550e68706f98dd1ef3c78b
Alex <avhoi83@gmail.com>
2024-12-14 15:37:35 -0800

    lint

--- src/api/realm/get-realms.ts
+++ src/api/realm/get-realms.ts
@@ -2,4 +2,7 @@ import { request } from '../base'
 import { Realm } from '.'
 
 // Public realms will be returned. If logged-in, the user's realm will also be returned.
-export const getRealms = () => request<Realm[]>('GET', 'realms').then(realms => realms.sort((a, b) => a.name.localeCompare(b.name)))
+export const getRealms = () =>
+  request<Realm[]>('GET', 'realms').then((realms) =>
+    realms.sort((a, b) => a.name.localeCompare(b.name)),
+  )
