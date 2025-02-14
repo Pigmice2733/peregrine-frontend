f31a4cebd45391dcba1dc24088b987561c6072d8
Alex <avhoi83@gmail.com>
2025-02-14 12:31:56 -0800

    undo unrelated changes

--- src/utils/move-focus-inside/index.ts
+++ src/utils/move-focus-inside/index.ts
@@ -4,7 +4,6 @@ const tababblesStr = tabbables.join(',')
 
 export const moveFocusInside = (el: HTMLElement) => {
   if (el.matches(tababblesStr)) return el.focus()
-  // eslint-disable-next-line no-warning-comments
   // TODO: Should this handle elements with modified tabindex, like https://github.com/theKashey/focus-lock/blob/master/src/utils/tabOrder.js
   const firstFocusableElement = el.querySelector(tababblesStr)
   if (firstFocusableElement) (firstFocusableElement as HTMLElement).focus()
